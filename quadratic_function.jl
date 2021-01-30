d2b221a6c537bf7a1c46594cf1c2dcb26595db55
William Simoni <56754601+WilliamSimoni@users.noreply.github.com>
2021-01-30 12:39:20 +0100

    deleted useless return

--- quadratic_function.jl
+++ quadratic_function.jl
@@ -3,13 +3,13 @@ using LinearAlgebra
 function positive_definite(x,y)
     Q = [6 -2; -2 6]
     q = [0 ; 0]
-    return return quadratic_function(x,y,Q,q)
+    return quadratic_function(x,y,Q,q)
 end
 
 function semi_positive_definite(x,y)
     Q = [0 1; 0 1]
     q = [0 ; 0]
-    return return quadratic_function(x,y,Q,q)
+    return quadratic_function(x,y,Q,q)
 end
 
 function not_positive_definite(x,y)
@@ -29,13 +29,13 @@ end
 function positive_definite_d(x,y)
     Q = [6 -2; -2 6]
     q = [0 ; 0]
-    return return quadratic_function_d(x,y,Q,q)
+    return quadratic_function_d(x,y,Q,q)
 end
 
 function semi_positive_definite_d(x,y)
     Q = [0 1; 0 1]
     q = [0 ; 0]
-    return return quadratic_function_d(x,y,Q,q)
+    return quadratic_function_d(x,y,Q,q)
 end
 
 function not_positive_definite_d(x,y)
